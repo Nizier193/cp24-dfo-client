1b391b9e9779d10f8af18cdbffd5043a2ef68e64
Nizier193 <nikita.borisov07@gmail.com>
2024-06-02 11:58:51 +0300

    Deleted all API keys

--- ML_Backend/shell.py
+++ ML_Backend/shell.py
@@ -9,16 +9,25 @@ text = input("<< Пожалуйста, скопируйте текст, кото
 if text in ['Нет', 'НЕТ', 'нет']:
     text = open('text.txt', 'r', encoding='utf-8').read()
 
-print('----------------')
+print()
+print('-------------------')
 print('Шаг 1. Генерация текстовой информации.')
 assembly = Banner_Assembly()
 result = assembly.make_text_content(text)
 
+def length():
+    return
+
 print()
 print('Из текста получилось выделить следующее.')
-print(f'    Мотивационная фраза: {result["inplace"]}')
-print(f'    Заголовок: {result["header"]}')
-print(f'    Основная мысль: {result["thesis"]}')
+l = len(result["inplace"].split())
+print(f'    Мотивационная фраза: {result["inplace"]} \n{'    Длина: ' + str(l) if l < 5 else ("    >> !!! Рекомендуется перегенерировать!")}\n')
+
+l = len(result["header"].split())
+print(f'    Заголовок: {result["header"]} \n{'    Длина: ' + str(l) if l < 5 else ("    >> !!! Рекомендуется перегенерировать!")}\n')
+
+l = len(result["thesis"].split())
+print(f'    Основная мысль: {result["thesis"]} \n{'    Длина: ' + str(l) if l < 10 else ("    >> !!! Рекомендуется перегенерировать!")}\n')
 
 print()
 ans = input("Вас устраивает текстовое содержание баннера? \n1 - Да \n2 - Нет \n>> ")
@@ -26,13 +35,23 @@ while ans not in ['ДА', 'Да', 'да', '1']:
     custom = input("Напишите, что можно было бы добавить или передать нейросети? \n>> ")
     result = assembly.make_text_content(f'{custom} \n {text}')
 
-    print(f'    Мотивационная фраза: {result["inplace"]}')
-    print(f'    Заголовок: {result["header"]}')
-    print(f'    Основная мысль: {result["thesis"]}')
+    print('Из текста получилось выделить следующее.')
 
-    ans = input("Вас устраивает текстовое содержание баннера? \n1 - Да \n2 - Нет \n>> ")
+    l = len(result["inplace"].split())
+    print(
+        f'    Мотивационная фраза: {result["inplace"]} \n{'    Длина: ' + str(l) if l < 5 else ("    >> !!! Рекомендуется перегенерировать!")}\n')
+
+    l = len(result["header"].split())
+    print(f'    Заголовок: {result["header"]} \n{'    Длина: ' + str(l) if l < 5 else ("     >> !!! Рекомендуется перегенерировать!")}\n')
+
+    l = len(result["thesis"].split())
+    print(
+        f'    Основная мысль: {result["thesis"]} \n{'    Длина: ' + str(l) if l < 12 else ("     >> !!! Рекомендуется перегенерировать!")}\n')
+
+    ans = input("Вас устраивает текстовое содержание баннера? \n1 - Да \n2 - Нет (перегенерировать)\n>> ")
 
 print()
+print('-------------------')
 print('Шаг 2. Генерация графической части. >> (Фон и основная картинка.)')
 images = assembly.make_content()
 images[0].show()
@@ -52,12 +71,13 @@ print('На этом этапе вам необходимо указать, ка
 FILEFORMATS = ['png', 'jpg', 'jpeg']
 
 QR = input('  Хотите добавить QR-код? (Да/Нет)  >> ')
-if QR in ['ДА', 'Да', 'да', '1']: print('\n< Важно!!! Поскольку вы хотите добавить QR-код, добавьте его в папку Project/dist с именем QR.png. >\n')
+q_flag = False
+if QR in ['ДА', 'Да', 'да', '1']: print('\n< Важно!!! Поскольку вы хотите добавить QR-код, добавьте его в папку Project/dist с именем QR.png. >\n'); q_flag = True
 assembly.stack_banner(
     thesis=True if input('  Хотите добавить тезис в две-три строки ниже заголовка? (Да/Нет) >> ') in ['ДА', 'Да', 'да', '1'] else False,
     inplace=True if input('  Хотите добавить мотивационную табличка "врез" сверху от заголовка? (Да/Нет) >> ') in ['ДА', 'Да', 'да', '1'] else False,
     button=True if input('  Хотите добавить кнопку "Узнать больше" ниже тезиса? (Да/Нет) >> ') in ['ДА', 'Да', 'да', '1'] else False,
-    QR=QR if QR.split('.')[-1].split()[0] in FILEFORMATS else None,
+    QR= 'QR.png' if q_flag else None,
 )
 
 print('Сборка успешна! Ознакомьтесь с полученным баннером.\nФормат - 1920x1080.\n\nСпасибо вам!')
